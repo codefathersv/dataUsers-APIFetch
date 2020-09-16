25915afeb4ea430bab18be2258aa763ef28c4c5c
alexischavezweb <alexischavezweb@gmail.com>
2020-09-16 16:06:35 -0600

    Peticiones AJAX con API Fetch, Datos de la API JSONPlaceholder

---
+++ js/app.js
@@ -0,0 +1,42 @@
+(() => {
+
+    const $datos = document.getElementById("datos");
+
+    $datos.addEventListener("click", (e) => {
+        const $filas = document.getElementById("filas");
+
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => {
+
+                // console.log(res);
+                // se convierte a formato json
+                return res.ok ? res.json() : Promise.reject(res);
+
+            })
+            .then((json) => {
+
+                json.forEach(el => {
+                    $filas.innerHTML += `
+                        <tr>
+                            <td>${el.name}</td>
+                            <td>${el.email}</td>
+                            <td>${el.phone}</td>
+                        </tr>
+                    `;
+                })
+
+            })
+            .catch((err) => {
+
+                let message = err.statusText || "Ocurrio un error";
+                $filas.innerHTML = `
+                    <tr>
+                        <td colspan="3">Error ${err.status}: ${message}</td>
+                    </tr>
+                `;
+
+            })
+            .finally();
+    })
+
+})();
